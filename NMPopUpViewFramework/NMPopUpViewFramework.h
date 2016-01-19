717718491a0a79d9caff2bb8e2a766c4b0bfa8cc
Taylor Martin <taylormartin.uga@gmail.com>
2016-01-19 13:19:34 -0500

    added shared framework scheme

---
+++ NMPopUpViewFramework/NMPopUpViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  NMPopUpViewFramework.h
+//  NMPopUpViewFramework
+//
+//  Created by Taylor Martin on 1/19/16.
+//  Copyright © 2016 Make and Build. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NMPopUpViewFramework.
+FOUNDATION_EXPORT double NMPopUpViewFrameworkVersionNumber;
+
+//! Project version string for NMPopUpViewFramework.
+FOUNDATION_EXPORT const unsigned char NMPopUpViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NMPopUpViewFramework/PublicHeader.h>
+
+
